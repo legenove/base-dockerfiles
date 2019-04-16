4388a6031dbd0a3a5d3531834d07b9882f26789f
liuguoyang@cmcm.com <liuguoyang@cmcm.com>
2019-04-16 15:06:17 +0800

    add grpc cache

--- go/web1.11/main.go
+++ go/web1.11/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/gin"
-	_ "github.com/spf13/viper"
-	_ "github.com/fsnotify/fsnotify"
-	_ "github.com/jinzhu/gorm"
-	_ "go.uber.org/zap"
-	_ "go.uber.org/zap/zapcore"
-	_ "github.com/json-iterator/go"
 	_ "github.com/aws/aws-sdk-go/aws"
 	_ "github.com/aws/aws-sdk-go/aws/session"
 	_ "github.com/aws/aws-sdk-go/service/sqs"
+	_ "github.com/fsnotify/fsnotify"
 	_ "github.com/garyburd/redigo/redis"
+	_ "github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm"
+	_ "github.com/json-iterator/go"
 	_ "github.com/nu7hatch/gouuid"
 	_ "github.com/rs/xid"
+	_ "github.com/spf13/viper"
+	_ "go.uber.org/zap"
+	_ "go.uber.org/zap/zapcore"
+	_ "google.golang.org/grpc"
 )
 
 func main() {
